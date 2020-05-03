d11878c4a3935b57b32a6e6df30211e52aebf0f2
Kyler Wong <wsm.kyler@gmail.com>
2020-05-03 18:36:58 +0800

    Updates

---
+++ precache-manifest.59e18359f127bb7acb1fae14a2e6ff66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e18c4ee713848929010a7522ca2ad84f",
+    "url": "/todo-redux/index.html"
+  },
+  {
+    "revision": "45ddcc1ec0d0fc04fbac",
+    "url": "/todo-redux/static/js/2.f28192d4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-redux/static/js/2.f28192d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a9643555e4e555601c6",
+    "url": "/todo-redux/static/js/main.0a57477e.chunk.js"
+  },
+  {
+    "revision": "52d6833e57709d24f9ea",
+    "url": "/todo-redux/static/js/runtime-main.d0fb790c.js"
+  }
+]);
